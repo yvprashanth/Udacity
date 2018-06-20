10dfa39a04c09ea5ea900fa45fb5c80757b91282
Prashanth <yvprashanth@gmail.com>
2018-06-19 21:19:51 -0700

    More changes

--- my-readable/src/index.js
+++ my-readable/src/index.js
@@ -1,8 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { BrowserRouter } from 'react-router-dom'
+import App from './components/App'
+import store from './store/'
+import { Provider } from 'react-redux'
+import registerServiceWorker from './registerServiceWorker'
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Provider store={ store }>
+    <MuiThemeProvider>
+      <BrowserRouter>
+        <App/>
+      </BrowserRouter>
+    </MuiThemeProvider>
+  </Provider>,
+  document.getElementById('root')
+);
+
 registerServiceWorker();
